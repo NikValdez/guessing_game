38d1065c7dd4908d44d6c0f59bc7ecf318e2b4fa
Nik <nerdvestor@gmail.com>
2017-02-07 20:50:33 -0800

    first commit

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-get '/' do 
-	number = rand(100)
-	erb :index, :locals => {:number => number}
+NUMBER = rand(100)
+
+
+get '/' do
+  guess = params['guess'].to_i
+  message, background = check_guess(guess)
+  erb :index, :locals => {:number => NUMBER, :message => message, :background => background}
+end
+
+def check_guess(guess)
+  if guess - NUMBER > 5
+    ["Way too high!", 'red']
+  elsif guess > NUMBER
+    ["Too high!", 'OrangeRed']
+  elsif NUMBER - guess > 5
+    ["Way too low!", 'red']
+  elsif guess < NUMBER
+    ["Too low!", 'OrangeRed']
+  elsif guess == NUMBER
+    ["Good job! The secret number is #{guess}", 'green']
+  end
 end
